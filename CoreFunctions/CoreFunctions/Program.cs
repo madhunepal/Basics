96dee17cbcb609ad2a7cc3ab364b3a24bbfd46d6
madhunepal <madhunepal@gmail.com>
2018-02-02 17:48:43 -0500

    Desktop display button and message added

---
+++ CoreFunctions/CoreFunctions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inter 2 integers");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
