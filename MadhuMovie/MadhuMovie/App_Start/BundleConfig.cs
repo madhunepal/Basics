a2483eefabe9e0c2aecc75ef00b7f0e81396e5cd
madhunepal <madhunepal@gmail.com>
2018-02-06 18:21:30 -0500

    index page is set

--- MadhuMovie/MadhuMovie/App_Start/BundleConfig.cs
+++ MadhuMovie/MadhuMovie/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MadhuMovie
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap_superhero.css",
                       "~/Content/site.css"));
         }
     }
---
+++ MadhuMovie/MadhuMovie/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MadhuMovie.Models;
+
+namespace MadhuMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Love" };
+            return View("movie");
+        }
+    }
+}
